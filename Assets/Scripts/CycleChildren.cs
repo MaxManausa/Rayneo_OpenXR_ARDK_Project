647e3bc1c50f894fb1203a2c0f8ff7b2ff182056
Max-Manausa <maxmanausa@gmail.com>
2025-07-06 19:44:58 +0800

    slight change: now number of total artwork pieces are visible to user

--- Assets/Scripts/CycleChildren.cs
+++ Assets/Scripts/CycleChildren.cs
@@ -61,7 +61,7 @@ public class CycleChildren : MonoBehaviour
     {
         if (artworkText != null)
         {
-            artworkText.text = "Artwork Piece: " + artworkNumber + "/43";
+            artworkText.text = "Artwork Piece: " + artworkNumber + "/" + transform.childCount;
         }
         else
         {
